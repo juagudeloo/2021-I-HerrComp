ba8d17ee7b04cdaacaa46a7627afe852a467c002
Juan Esteban Agudelo Ortiz <juagudeloo@unal.edu.co>
2021-03-05 13:03:49 -0500

    Intento de programa que no terminé y tampoco es correcto porque la relación de recurrencia no nos beneficia

---
+++ 2021-03-05-subcancellation/suminv.cpp
@@ -0,0 +1,20 @@
+//LA RELACIÓN DE RECURRENCIA
+//USADA NO NOS BENEFICIA. VER EL PROGRAMA HECHO POR EL PROFESOR.
+
+
+#include <iostream>
+#include <cmath>
+
+int main ()
+{
+    float sum1 = 0.0;
+    float sum2 = 0.0;
+
+    for(int ii = 1; ii <= N; ii++)
+    {
+        sum1 = sum1/(1+sum1);
+        sum2 = sum2/(1+sum2);
+    }
+//NO ESTÁ TERMINADO, PERO PARA NADA.
+
+}
